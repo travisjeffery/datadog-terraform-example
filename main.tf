df061a4733d50ad31973420825d42b5c66c25631
Travis Jeffery <tj@travisjeffery.com>
2017-08-24 02:44:29 -0500

    add docs and run fmt

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "datadog" {
 variable "datadog_api_key" {}
 variable "datadog_app_key" {}
 
+# Footer added to each monitor alert.
 variable "datadog_alert_footer" {
   default = <<EOF
 {{#is_no_data}}Not receiving data{{/is_no_data}}
@@ -15,25 +16,28 @@ variable "datadog_alert_footer" {
 EOF
 }
 
+# Trigger a separate alert for each host and env.
 variable "trigger_by" {
   default = "{host,env}"
 }
 
+# Variable defining the query and threshold shared by a monitor and graph for the disk usage.
 variable "disk_usage" {
   type = "map"
 
   default = {
-    query = "max:system.disk.in_use"
+    query     = "max:system.disk.in_use"
     threshold = "85"
   }
 }
 
+# Monitor for the the disk usage.
 resource "datadog_monitor" "disk_usage" {
-  name  = "Disk usage high"
-  query = "avg(last_5m):${var.disk_usage["query"]}{*} by ${var.trigger_by} * 100 > ${var.disk_usage["threshold"]}"
-  type  = "metric alert"
+  name           = "Disk usage high"
+  query          = "avg(last_5m):${var.disk_usage["query"]}{*} by ${var.trigger_by} * 100 > ${var.disk_usage["threshold"]}"
+  type           = "metric alert"
   notify_no_data = true
-  include_tags = true
+  include_tags   = true
 
   message = <<EOM
 Disk usage high: {{value}}
@@ -42,21 +46,23 @@ ${var.datadog_alert_footer}
 EOM
 }
 
-
+# Variable defining the query and threshold shared by a monitor and graph for the CPU usage.
 variable "cpu_usage" {
   type = "map"
 
   default = {
-    query = "avg:aws.ec2.cpuutilization"
+    query     = "avg:aws.ec2.cpuutilization"
     threshold = "85"
   }
 }
+
+# Monitor for the the CPU usage.
 resource "datadog_monitor" "cpu_usage" {
-  name  = "CPU usage high"
-  query = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
-  type  = "query alert"
+  name           = "CPU usage high"
+  query          = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
+  type           = "query alert"
   notify_no_data = true
-  include_tags = true
+  include_tags   = true
 
   message = <<EOM
 CPU usage high: {{value}}
@@ -65,42 +71,43 @@ ${var.datadog_alert_footer}
 EOM
 }
 
+# Timeboard including graphs of the disk and CPU usage, each graph includes a marker incidicating the metric's monitor alert threshold.
 resource "datadog_timeboard" "host_metrics" {
-  title = "Host metrics"
+  title       = "Host metrics"
   description = "Host level metrics: CPU, memory, disk, etc."
-  read_only = true
+  read_only   = true
 
   graph {
-    title = "CPU usage"
-    viz = "timeseries"
+    title     = "CPU usage"
+    viz       = "timeseries"
     autoscale = true
 
     request {
-      q = "${var.cpu_usage["query"]}{*} by ${var.trigger_by}"
+      q          = "${var.cpu_usage["query"]}{*} by ${var.trigger_by}"
       aggregator = "avg"
-      type = "line"
+      type       = "line"
     }
 
     marker {
       value = "y > ${var.cpu_usage["threshold"]}"
-      type = "error dashed"
+      type  = "error dashed"
     }
   }
 
   graph {
-    title = "Disk usage"
-    viz = "timeseries"
+    title     = "Disk usage"
+    viz       = "timeseries"
     autoscale = true
 
     request {
-      q = "${var.disk_usage["query"]}{*} by ${var.trigger_by}"
+      q          = "${var.disk_usage["query"]}{*} by ${var.trigger_by}"
       aggregator = "avg"
-      type = "line"
+      type       = "line"
     }
 
     marker {
       value = "y > ${var.disk_usage["threshold"]}"
-      type = "error dashed"
+      type  = "error dashed"
     }
   }
 }
